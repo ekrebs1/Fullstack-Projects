d0cafff7e61e0fd7c13af3c25ede82f0866326dd
Erica Krebs <67703792+ekrebs1@users.noreply.github.com>
2021-04-10 16:41:19 -0500

    Project_10 Fin!

---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+const { PORT = 3000, WEATHER_KEY } = process.env;
+
+const express = require('express');
+const server = express();
+
+const axios = require('axios');
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.post('/job-search', async (req, res) => {
+  try {
+    const { description, fulltime } = req.body;
+
+    const URL = `https://jobs.github.com/positions.json?${
+      description ? `description=${ description }&` : ''
+    }${
+      fulltime ? 'fulltime=true' : ''
+    }`;
+
+    const { data } = await axios.get(URL);
+    
+
+    res.send({ results: data });
+  } catch (error) {
+    res.send({ error });
+  }
+});
+
+const cowsay = require('cowsay');
+const Quote = require('inspirational-quotes')
+
+server.get('/cowspiration', (req, res) => {
+  const { text, author } = Quote.getQuote();
+
+  const cow = cowsay.say({
+    text: `${ text }\n\n- ${ author }`,
+    W: 80,
+  });
+
+  res.send({ cow });
+});
+
+server.get('/weather', async (req, res) => {
+  try {
+    const { lat, lon } = req.query;
+    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&appid=${ WEATHER_KEY }`;
+
+    const { data } = await axios.get(URL);
+    res.send({ results: data });
+  } catch (error) {
+    res.send({ error });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log('I am listening...');
+});
