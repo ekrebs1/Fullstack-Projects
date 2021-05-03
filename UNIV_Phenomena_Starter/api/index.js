64325c214b4a33a277ce371674deba945b0819ef
Erica Krebs <67703792+ekrebs1@users.noreply.github.com>
2021-05-02 20:13:59 -0500

    Add files via upload

---
+++ UNIV_Phenomena_Starter/api/index.js
@@ -0,0 +1,86 @@
+// Build an apiRouter using express Router
+const express = require('express');
+const apiRouter = express.Router();
+
+// Import the database adapter functions from the db
+const { createReport, 
+        getOpenReports, 
+        closeReport, 
+        createReportComment 
+    }= require("../db");
+
+/**
+ * Set up a GET request for /reports
+ * 
+ * - it should use an async function
+ * - it should await a call to getOpenReports
+ * - on success, it should send back an object like { reports: theReports }
+ * - on caught error, call next(error)
+ */
+ apiRouter.get("/reports", async (req, res, next) =>{
+    try{
+        const getReports = await getOpenReports()
+        res.send({reports: getReports})
+    }catch(err){
+        next(err)
+    }
+})
+
+
+/**
+ * Set up a POST request for /reports
+ * 
+ * - it should use an async function
+ * - it should await a call to createReport, passing in the fields from req.body
+ * - on success, it should send back the object returned by createReport
+ * - on caught error, call next(error)
+ */
+apiRouter.post('/reports', async (req, res, next) => {
+    try{  
+        const newReport = await createReport( req.body )
+        res.send(newReport)
+    }catch(error){
+        next(error)
+    }
+});
+
+
+/**
+ * Set up a DELETE request for /reports/:reportId
+ * 
+ * - it should use an async function
+ * - it should await a call to closeReport, passing in the reportId from req.params
+ *   and the password from req.body
+ * - on success, it should send back the object returned by closeReport
+ * - on caught error, call next(error)
+ */
+apiRouter.delete('/reports/:reportId', async (req, res, next) => {
+    try{
+        const deleteReport = await closeReport(req.params.reportId, req.body.password)
+        res.send(deleteReport)
+    }catch(error){
+        next(error)
+    }
+});
+
+/**
+ * Set up a POST request for /reports/:reportId/comments
+ * 
+ * - it should use an async function
+ * - it should await a call to createReportComment, passing in the reportId and
+ *   the fields from req.body
+ * - on success, it should send back the object returned by createReportComment
+ * - on caught error, call next(error)
+ */
+apiRouter.post('/reports/:reportId/comments', async (req, res, next) => {
+    try{
+        const postComments = await createReportComment(req.params.reportId, req.body);
+        res.send(postComments)
+    }catch(error){
+        next(error)
+    }
+});
+
+
+// Export the apiRouter
+module.exports= apiRouter;
